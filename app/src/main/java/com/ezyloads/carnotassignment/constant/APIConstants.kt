74d76bf533fa959dec0959ed126b4212e2e5ed88
NikhilYAdavTranso <nikhil.yadav@transo.in>
2022-09-11 11:41:37 +0530

    First

--- app/src/main/java/com/ezyloads/carnotassignment/constant/APIConstants.kt
+++ app/src/main/java/com/ezyloads/carnotassignment/constant/APIConstants.kt
@@ -7,4 +7,5 @@ package com.ezyloads.carnotassignment.constant
 object APIConstants {
     const val RESOURCES = "resource/9ef84268-d588-465a-a308-a864a43d0070"
     const val RESPONSE_FORMAT = "json"
+
 }
